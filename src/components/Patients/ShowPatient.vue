1a29130bf8295362407236659bb8ee872868bb0e
Smembe812 <psemberekajr@gmail.com>
2019-04-15 09:49:27 +0200

    patient steps

--- src/components/Patients/ShowPatient.vue
+++ src/components/Patients/ShowPatient.vue
@@ -41,42 +41,105 @@
         
     </section>
 
+    <section>
+        <div class="container">
+                <h5 class="text-align-center">Patient Steps</h5>
+                <div class="form-row">
+                    
+                    <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Available Steps</label>
+                                <select class="form-control">
+                                    <option :value="null" disabled>All The Available Stages</option>
+                                    <option v-for="(stage,index) in stages" v-bind:key="index" :value="stage.step">{{stage.step}}</option>
+                                </select>
+                            </div>
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-center">
+                            <button class="btn btn-primary btn-lg my-4" v-b-modal.stage-modal>Add New Stage</button>
+                    </div>
+                </div>
+        </div>
+        
+    </section>
+
         <section class="search-results">
         <div class="container">
             <div class="row d-flex justify-content-center py-5">
-                 
-                    
-                    <table class="table">
-                            <thead class="thead-dark">
-                              <tr>
-                                <th scope="col">No.</th>
-                                <th scope="col">Mastercard</th>
-                                <th scope="col">Date Created</th>
-                                <th scope="col">Actions</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr v-for="(patientCard, index) in patientCards" v-bind:key="index">
-                                <th scope="row">{{ index+1}}</th>
-                                <td>{{ patientCard.masterCard.name}}</td>
-                                <td> {{ patientCard.dateCreated}}</td>
-                                <td><button type="button" class="btn btn-success btn-sm" v-on:click="setPatientCard(patientCard)">View MasterCards</button></td>
-                              </tr>
-                            </tbody>
-                          </table>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Mastercard</th>
+                        <th scope="col">Date Created</th>
+                        <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(patientCard, index) in patientCards" v-bind:key="index">
+                        <th scope="row">{{ index+1}}</th>
+                        <td>{{ patientCard.masterCard.name}}</td>
+                        <td> {{ patientCard.dateCreated}}</td>
+                        <td><button type="button" class="btn btn-success btn-sm" v-on:click="setPatientCard(patientCard)">View MasterCards</button></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div> 
         </div>
         
     </section>
+
+    <b-modal id="stage-modal" title="Add New Step" hide-footer	 size="lg">
+           <form name='addStep' v-on:submit.prevent="addStep">
+            <div class="form-row">
+                <div class="col-md-12 mb-3">
+                    <label>ART Number</label>
+                    <input type="name" class="form-control" v-model="art_number" required>
+                
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-md-12 mb-3">
+                    <label >Step Date</label>
+                    <input type="date" class="form-control" v-model="stepDate" required>
+                
+                </div>
+            </div>
+              <div class="form-row">
+                  <div class="col-md-12 mb-3">
+                      <label>Site</label>
+                      <input type="text" class="form-control" placeholder="Site" v-model="site" required>
+                  </div>
+              </div>
+            <div class="form-row">
+                <div class="col-md-12 mb-3">
+                        <label>Step</label>
+                        <select  v-model="step" class="form-control" placeholder="Stape" >
+                            <option :value="null" disabled>All The Available Step</option>
+                            <option value="Art Start">Art Start</option>
+                            <option value="Trans-in">Trans-in</option>
+                            <option value="Trans-out">Trans-out</option>
+                        </select>
+                </div>
+            </div>
+            <div class="form-row">
+                    <div class="col-md-12 mb-3">
+                            <label>Original Destination</label>
+                            <input type="text" class="form-control" placeholder="Original Destination" v-model="original_destination">
+                    </div>
+                </div>
+                <button class="btn btn-success" type="submit">Submit form</button>
+            </form>
+        
+    </b-modal>
     </div>
 </template>
 
 <script>
 
     import NavBar from "../../views/NavBar";
-    import authResource from './../../authResource';
-    import _ from 'lodash'
-import { version } from 'punycode';
+    import authResource from './../../authResource'
+    import { version } from 'punycode';
 
     export default {
         name: 'ShowPatient',
@@ -89,7 +152,6 @@ import { version } from 'punycode';
                 authResource().get(dhisAPIEndpoint)
                     .then((response)=>{
                         this.availableMasterCards.push(...response.data.data)
-                        console.log(this.availableMasterCards)
                     })
                     .catch((error)=>{
                         console.log(error)
@@ -120,6 +182,33 @@ import { version } from 'punycode';
                 sessionStorage.setItem('patientCard', JSON.stringify(patientCard))
 
                 this.$router.push('/patients/show/card')
+            },
+            getStages(){
+                const url = `${this.APIHosts.art}/patients/${this.patient.patientID}/steps`
+
+                authResource().get(url)
+                    .then(({data: {data}})=> {
+                        this.stages = data;
+                        console.log(this.stages[0].patientStepID)
+                    })
+                    .catch(err => console.error(err))
+            },
+            addStep(){
+                const payload = {
+                    art_number: this.art_number,
+                    date: this.stepDate,
+                    site: this.site,
+                    step: this.step,
+                    original_destination: this.original_destination,
+                    patient: this.patient.patientID,
+                }
+                const url = `${this.APIHosts.art}/patient-steps`
+                console.log(payload)
+                authResource().post(url, payload)
+                    .then(({data: {data}})=> {
+                        this.stages.push(data)
+                    })
+                    .catch(err => console.error(err))
             }
         },
         data: () => {
@@ -131,16 +220,17 @@ import { version } from 'punycode';
                         personAddress : {}
                     }
                 },
-                availableMasterCards : [
 
-                ],
-                patientCards : [
-
-                ],
-                masterCardsToShow : [
-
-                ],
-                selectedMasterCardVersion : null
+                art_number:'',
+                step:'',
+                stepDate:'',
+                site:'',
+                original_destination: '',
+                availableMasterCards : [],
+                patientCards : [],
+                masterCardsToShow : [],
+                selectedMasterCardVersion : null,
+                stages: []
             }
         },
         created() {
@@ -157,7 +247,7 @@ import { version } from 'punycode';
 
         watch : {
             patient : function (value) {
-
+                this.getStages()
                 let dhisAPIEndpoint = `${this.APIHosts.art}/${this.BASE_URL}/${value.patientID}/cards`;
 
                 authResource().get(dhisAPIEndpoint)
