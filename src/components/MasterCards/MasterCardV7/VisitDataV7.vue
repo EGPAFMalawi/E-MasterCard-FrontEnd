15a46b542e8ec3ea2abfc1bf3151b9f135d3b72d
Smembe812 <psemberekajr@gmail.com>
2019-05-13 14:00:36 +0200

    updated v7 visit inputs dates

--- src/components/MasterCards/MasterCardV7/VisitDataV7.vue
+++ src/components/MasterCards/MasterCardV7/VisitDataV7.vue
@@ -303,7 +303,7 @@
                         <input v-model="concepts.concept46" class="form-control tb-form"  type="number" min="0" step="1" oninput="validity.valid||(value='');">
                     </td>
                     <td>
-                        <input id="tooltip-button-1" v-model="concepts.concept47" class="form-control tb-form"  type="date" >
+                        <input id="tooltip-button-1" v-model="concepts.concept47" ref="appointmentDate" class="form-control tb-form"  type="date" >
                         <span>{{ errors.first('Next Visit')}}</span>
                     </td>
                 </tr>
@@ -510,6 +510,19 @@
                 nextApponintmentDate.setDate(fromDate.getDate() + givenPillsInt)
 
                 return nextApponintmentDate.toISOString().split('T')[0]
+            },
+             calculateMaxStartDate(){
+                const today = new Date()
+                const max = new Date(today.setDate(today.getDate() + 365))
+                return max.toISOString().split('T')[0]
+            },
+            setMinMax(ref){
+                const startDate = localStorage.getItem('startDate')
+                
+                if(startDate !== ''){
+                    this.$refs[ref].setAttribute('min', startDate)
+                    this.$refs[ref].setAttribute('max', this.calculateMaxStartDate())
+                }
             }
         },
         data: () => {
@@ -581,6 +594,8 @@
             },
             patientCardData : function (value) {
                 this.fillConceptObservations(value);
+                this.setMinMax('visitDate')
+                this.setMinMax('appointmentDate')
             },
             'concepts.concept32': function(){
                 if(this.concepts.concept32!=='' && this.concepts.concept47!=='')
