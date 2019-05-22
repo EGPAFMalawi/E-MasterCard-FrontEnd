76d639fbc27b64aa3f6c47e3471c7b1073500db9
Smembe812 <psemberekajr@gmail.com>
2019-05-22 20:08:34 +0200

    next appointment date in range

--- src/components/MasterCards/MasterCardV5/VisitDataV5.vue
+++ src/components/MasterCards/MasterCardV5/VisitDataV5.vue
@@ -339,7 +339,7 @@
                             <input v-model="concepts.concept46" class="form-control tb-form"  type="number" min="0" step="1" oninput="validity.valid||(value='');">
                         </td>
                         <td>
-                            <input id="tooltip-button-1" v-model="concepts.concept47" class="form-control tb-form"  type="date" required>
+                            <input ref="appointmentDate" id="tooltip-button-1" v-model="concepts.concept47" class="form-control tb-form"  type="date" required>
                             <span>{{ errors.first('Next Visit')}}</span>
                         </td>
                     </tr>
