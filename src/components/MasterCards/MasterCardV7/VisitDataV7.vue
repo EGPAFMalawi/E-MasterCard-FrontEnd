06a7c88fc6f209faad355d0c5423211dd445dd05
Smembe812 <psemberekajr@gmail.com>
2019-08-31 15:58:29 +0200

    aldult visits/outcomes fix

--- src/components/MasterCards/MasterCardV7/VisitDataV7.vue
+++ src/components/MasterCards/MasterCardV7/VisitDataV7.vue
@@ -720,7 +720,8 @@
             disableNextAppointment(e, ob){
                 if (this.nextAppointmentDateEdit === null)
                     this.nextAppointmentDateEdit = this.observations[ob].value
-                const appointmentOb = e.target.parentNode.parentNode.children[17].children[0]
+                const appointmentOb = e.target.parentNode.parentNode.children[16].children[0]
+                
                 if (e.target.value !== '' && e.target.value !== 'Blank'){
                     appointmentOb.disabled = true
                     appointmentOb.value = ''
